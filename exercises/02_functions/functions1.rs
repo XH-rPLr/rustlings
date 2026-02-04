46ba27ed39c8c24084b4931c886616b6607b82b1
Joy Kim <joyk413@gmail.com>
2026-02-03 20:27:05 -0800

    chapter 2: functions

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -1,5 +1,11 @@
 // TODO: Add some function with the name `call_me` without arguments or a return value.
 
+use std::fmt::Error;
+
 fn main() {
     call_me(); // Don't change this line
+}
+
+fn call_me() -> Result<(), Error> {
+    Ok(())
 }
